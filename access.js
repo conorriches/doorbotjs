e5d5804d5563fdb528ed599a93ff6b796a438f8c
Conor Riches <github@conorriches.co.uk>
2022-11-26 23:31:46 +0000

    rising edge

--- access.js
+++ access.js
@@ -88,10 +88,10 @@ telegram.announceStartup();
  */
 const gpio_relay_1 = new Gpio(p_relay_1, "out");
 const gpio_relay_2 = new Gpio(p_relay_2, "out");
-const gpio_doorbell = new Gpio(p_input_doorbell, "in", {
+const gpio_doorbell = new Gpio(p_input_doorbell, "in", "rising", {
   debounceTimeout: 10,
 });
-const gpio_rex = new Gpio(p_input_rex, "in", {
+const gpio_rex = new Gpio(p_input_rex, "in", "rising", {
   debounceTimeout: 10,
 });
 const gpio_rfid_beep = new Gpio(p_rfid_beep, "out");
